5b2dcbea47296354cb0f03d1131308d77725a302
Artur <ar2r.world@gmail.com>
2024-04-06 17:32:06 -0400

    simplify healthhandler

--- app/server/server.go
+++ app/server/server.go
@@ -5,24 +5,22 @@ import (
 )
 
 type Server struct {
-	port             string
-	dir              string
-	revision         string
-	HealthController *HealthController
+	port     string
+	dir      string
+	revision string
 }
 
 func NewServer(port, dir, rev string) *Server {
 	return &Server{
-		port:             port,
-		dir:              dir,
-		revision:         rev,
-		HealthController: NewHealthController(dir),
+		port:     port,
+		dir:      dir,
+		revision: rev,
 	}
 }
 
 func (s *Server) Start() {
 	http.Handle("/download/", http.StripPrefix("/download/", http.FileServer(http.Dir(s.dir))))
-	http.HandleFunc("/health", s.Health)
+	http.HandleFunc("/health", s.HealthHandler)
 	http.HandleFunc("/records", s.Index)
 	http.ListenAndServe(":"+s.port, nil) //nolint:errcheck,gosec
 }
